e8fc04b1233afdb38f3aa2f796de0217946f5eb1
wolf <itsthewolfprogrammer@gmail.com>
2026-03-30 14:49:12 +0530

    wrote atmosphere.py

--- core/atmosphere.py
+++ core/atmosphere.py
@@ -1,1 +1,12 @@
 # air_density, US std atm
+import numpy as np
+
+RHO0 = 1.225 # kg/m^3
+Hs = 8500.0 # m
+G0 = 9.80665 # m/s^2
+
+def air_density(h):
+    return RHO0 * np.exp(-h / Hs)
+
+def gravity_force(m):
+    return np.array([0., 0., -m * G0], dtype=float) #this in in NED, and Z is positive upwards, so gravity is negative in Z direction
