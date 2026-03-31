a6b05feeaae71755ac5ba816bcd9df298f395ded
wolf <itsthewolfprogrammer@gmail.com>
2026-03-31 19:12:53 +0530

    Updated atmosphere.py

--- core/atmosphere.py
+++ core/atmosphere.py
@@ -3,7 +3,7 @@ import numpy as np
 
 RHO0 = 1.225 # kg/m^3
 Hs = 8500.0 # m
-G0 = 9.80665 # m/s^2
+G0 = 9.80665 # m/s^2 comes with upto ~3% error, under 100km altitude, but we can ignore that for now, since we are not doing precision landing or something like that. We can also ignore the variation of G0 with altitude, since it is very small compared to the variation of air density with altitude.
 
 def air_density(h):
     return RHO0 * np.exp(-h / Hs)
