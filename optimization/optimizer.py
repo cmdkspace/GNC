ff33493b0cbf52a3f2de5cb2aa942002074598ff
wolf <itsthewolfprogrammer@gmail.com>
2026-05-05 13:45:56 +0530

    added the cost history for general analysis and reference

--- optimization/optimizer.py
+++ optimization/optimizer.py
@@ -6,14 +6,17 @@ from scipy.optimize import minimize
 from optimization.cost_function import cost_function
 
 def run_optimizer(theta0, bounds, params, sim_params, gains, q_ref):
+    J_history = []
     def J(theta):
-        return cost_function(theta, params, sim_params, gains, q_ref)
-    result = minimize(
-        fun = J,
-        x0 = theta0, 
-        method='L-BFGS-B', 
-        bounds=bounds, 
-        options={'maxiter' : 50}
-    )
+        val = cost_function(theta, params, sim_params, gains, q_ref)
+        J_history.append(val)
+        return val
 
-    return result.x, result.fun, result
+    result = minimize(
+        fun=J,
+        x0=theta0,
+        method='L-BFGS-B',
+        bounds=bounds,
+        options={'maxiter': 50}
+    )
+    return result.x, result.fun, result, J_history
